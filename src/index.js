09b1aac6e437293267373900f7e00da4de557c97
mbaqir8936 <n01307731@humbermail.ca>
2021-06-15 10:59:18 -0400

    added my story
    added my md file and built and served the website

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { addHomePage } from './homepage.js';
 import { resetPublicDir, moveContentToPostsDir } from './utilities.js';
 
 const posts = readdirSync(config.dev.postsdir)
-    .filter(file => extname(file).toLowerCase() === '.md')
-    .map(post => post.slice(0, -3))
-    .map(post => createPost(post))
-    .sort((a, b) =>  b.attributes.date - a.attributes.date);
+  .filter(file => extname(file).toLowerCase() === '.md')
+  .map(post => post.slice(0, -3))
+  .map(post => createPost(post))
+  .sort((a, b) => b.attributes.date - a.attributes.date);
 
 if (!existsSync(config.dev.outdir)) {
   mkdirSync(config.dev.outdir);
